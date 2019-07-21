79f1bab55190800c372c1dc55f7b1cd0781d6d76
Nikhil Nair <nair.n@husky.neu.edu>
2019-07-21 15:59:59 -0400

    Created basic profile component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {HomeComponent} from "./home/home.component";
+import {HomeComponent} from './home/home.component';
+import {ProfileComponent} from './profile/profile.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/home' },
   {path: 'home', component: HomeComponent},
+  {path: 'profile', component: ProfileComponent}
 
 
 ];
